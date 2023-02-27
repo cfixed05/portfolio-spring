b926b18264d96b494c2712b572debe870217cefc
Carlo <cfixed05@gmail.com>
2023-02-27 14:48:24 +0800

    Add files via upload

---
+++ src/main/java/portfolio/carlocastro/PortfolioApplication.java
@@ -0,0 +1,14 @@
+package portfolio.carlocastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+		System.out.println(System.currentTimeMillis());
+	}
+
+}
